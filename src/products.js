64156802c72035502b7f67a216aaf249ba04bd7c
KanishkaReddy0801 <krishnakanishkareddyalla@gmail.com>
2023-02-26 13:42:09 +0530

    adding Ecom web

---
+++ src/products.js
@@ -0,0 +1,59 @@
+import product1 from './assets/bag.png';
+import Product2 from './assets/heels.png';
+import product3 from './assets/iphone.png';
+import product4 from './assets/lipsstick.png';
+import product5 from './assets/nike.png';
+import product6 from './assets/perfume.png';
+import product7 from './assets/reddress.png';
+import product8 from './assets/skincare.png';
+
+export const PRODUCTS = [
+    {
+        id:1,
+        productName: "Bag",
+        price: 420000,
+        productImage:product1,
+    },
+    {
+        id:2,
+        productName: "Heels",
+        price: 80000,
+        productImage: Product2,
+    },
+    {
+        id:3,
+        productName: "Iphone",
+        price: 124999,
+        productImage:product3,
+    },
+    {
+        id:4,
+        productName: "Lipstick",
+        price: 4999,
+        productImage:product4,
+    },
+    {
+        id:5,
+        productName: "Shoes",
+        price: 149000,
+        productImage: product5
+    },
+    {
+        id:6,
+        productName: "Perfume",
+        price: 554999,
+        productImage: product6,
+    },
+    {
+        id:7,
+        productName: "Red Dress",
+        price: 450700,
+        productImage: product7,
+    },
+    {
+        id:8,
+        productName: "Skin Care",
+        price: 9999,
+        productImage: product8,
+    }
+];
